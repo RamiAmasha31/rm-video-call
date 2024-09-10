a353a27cfd4cb2ba890cd203d8a7d517b6726622
RamiAmasha31 <ramiamasha84@gmailcom>
2024-09-10 19:53:53 +0300

    updated fetchlogs

---
+++ video-call/api/fetchlogs.js
@@ -0,0 +1,56 @@
+// api/fetchLogs.js
+
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+export default async function handler(req, res) {
+  const { userId } = req.query;
+  try {
+    if (!userId) {
+      return res.status(400).json({ error: "User ID is required" });
+    }
+
+    const userQuery = query(
+      collection(db, "users"),
+      where("userId", "==", userId)
+    );
+    const querySnapshot = await getDocs(userQuery);
+
+    if (querySnapshot.empty) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const userDoc = querySnapshot.docs[0];
+    const userData = userDoc.data();
+    const logs = userData.logs || [];
+
+    res.status(200).json(logs);
+  } catch (error) {
+    console.error("Error fetching logs:", error.message, error.stack);
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+}
