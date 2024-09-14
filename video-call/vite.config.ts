8dbde988b3f3e03ed4f0a7a1c1e128ec71796621
RamiAmasha31 <ramiamasha84@gmailcom>
2024-09-14 19:20:47 +0300

    update the configurations

--- video-call/vite.config.ts
+++ video-call/vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
-  },
-  server: {
-    open: true, // Automatically open the browser on server start
-    // port: 3002, // Specify port if necessary
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./video-call"),
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      },
     },
   },
 });
